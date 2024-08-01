a7ab32002ec816222fdf4a35e937b04699c7bdfb
Menclet <clementlonde23@gmail.com>
2024-08-01 04:25:17 +0000

    Passage dans les colonnes et markage de Score gamestartmessage et gameovermessage

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from objects.background import Background
 from  objects.floor import Floor
 from objects.column import Column
 from objects.bird import Bird
+from objects.game_start_message import GameStartMessage
+from objects.game_over_message import GameOverMessage
 import assets
 import configs
 
@@ -13,40 +15,65 @@ screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
 
 colmn_create_event = pygame.USEREVENT
 clock = pygame.time.Clock()
-runnning = True
 
+runnning = True
+game_over = False
+score = 0
+game_start =False
 assets.load_sprites()
 
 sprites = pygame.sprite.LayeredUpdates()
 
+def create_sprites():
+    Background(0,sprites)
+    Background(1,sprites)
+    Floor(0,sprites)
+    Floor(1,sprites)
+    return  Bird(sprites),GameStartMessage(sprites)
 
-Background(0,sprites)
-Background(1,sprites)
-Floor(0,sprites)
-Floor(1,sprites)
+bird,game_started_message  = create_sprites()
 
-bird = Bird(sprites)
+
+#game_over_message =GameOverMessage(sprites)
 
 
 pygame.time.set_timer(colmn_create_event,1500)
-game_over = False
+
+
 while runnning:
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             runnning = False
         if event.type == colmn_create_event:
             Column(sprites)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not game_start and not  game_over:
+                game_start = True
+                game_started_message.kill()
+            if event.key == pygame.K_ESCAPE and game_over:
+                #create_sprites()
+                game_over = False
+                game_start =False
+                sprites.empty()
+                bird, game_started_message = create_sprites()
+
+
         bird.handle_event(event)
     screen.fill(0)
 
     sprites.draw(screen)
-    if not game_over:
+    if game_start and not game_over:
         sprites.update()
 
     if bird.check_collision(sprites):
         game_over =True
+        game_start = False
+        GameOverMessage(sprites)
 
-
+    for sprite in sprites:
+        if type(sprite) is Column and sprite.is_passed():
+            score +=1
+    print(score)
     pygame.display.flip()
 
     clock.tick(configs.FPS)
--- objects/column.py
+++ objects/column.py
@@ -16,12 +16,12 @@ class Column(pygame.sprite.Sprite):
         self.sprite_rect = self.sprite.get_rect()
 
         self.pipe_bottom = self.sprite
-        self.pipe_bottom_rect = self.pipe_bottom.get_rect(topleft=(0,self.sprite_rect.height + self.gap))
+        self.pipe_bottom_rect = self.pipe_bottom.get_rect(topleft=(0, self.sprite_rect.height + self.gap))
 
         self.pipe_top = pygame.transform.flip(self.sprite, False, True)
-        self.pipe_top_rect = self.pipe_top.get_rect(topleft=(0,5))
+        self.pipe_top_rect = self.pipe_top.get_rect(topleft=(0, 5))
 
-        self.image = pygame.surface.Surface((self.sprite_rect.width, self.sprite_rect.height*2 + self.gap),
+        self.image = pygame.surface.Surface((self.sprite_rect.width, self.sprite_rect.height * 2 + self.gap),
                                             pygame.SRCALPHA)
 
         self.image.blit(self.pipe_bottom, self.pipe_bottom_rect)
@@ -30,10 +30,10 @@ class Column(pygame.sprite.Sprite):
         sprite_floor_height = assets.get_sprite("floor2").get_rect().height
 
         min_y = 80
-        max_y = configs.SCREEN_HEIGHT- sprite_floor_height
-
+        max_y = configs.SCREEN_HEIGHT - sprite_floor_height
 
         self.rect = self.image.get_rect(midleft=(configs.SCREEN_WIDTH, random.uniform(min_y, max_y)))
+        self.passed = False
         self.mask = pygame.mask.from_surface(self.image)
         super().__init__(*groups)
 
@@ -41,3 +41,9 @@ class Column(pygame.sprite.Sprite):
         self.rect.x -= 3
         if self.rect.right <= 0:
             self.kill()
+
+    def is_passed(self):
+        if self.rect.x < 50 and not self.passed:
+            self.passed = True
+            return True
+        return False
